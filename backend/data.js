ea82d5f710244248a58057447ff9e9f196fbaffd
Dewruwan95 <dewruwanebay@gmail.com>
2022-05-22 12:23:40 +0530

    cadd product to mongodb and load them

--- backend/data.js
+++ backend/data.js
@@ -17,7 +17,6 @@ const data = {
   ],
   products: [
     {
-      _id: '1',
       name: 'Nike T- Shirt',
       category: 'shirt',
       image: '/img/products/p1.jpg',
@@ -29,7 +28,6 @@ const data = {
       description: 'High Quality Products',
     },
     {
-      _id: '2',
       name: 'Nike T- Shirt 2',
       category: 'shirt',
       image: '/img/products/p2.jpg',
@@ -41,7 +39,6 @@ const data = {
       description: 'High Quality Products',
     },
     {
-      _id: '3',
       name: 'Nike T- Shirt 3',
       category: 'shirt',
       image: '/img/products/p3.jpg',
@@ -53,7 +50,6 @@ const data = {
       description: 'High Quality Products',
     },
     {
-      _id: '4',
       name: 'Nike T- Shirt 4',
       category: 'shirt',
       image: '/img/products/p4.jpg',
@@ -65,7 +61,6 @@ const data = {
       description: 'High Quality Products',
     },
     {
-      _id: '5',
       name: 'Nike T- Shirt 5',
       category: 'shirt',
       image: '/img/products/p5.jpg',
@@ -77,7 +72,6 @@ const data = {
       description: 'High Quality Products',
     },
     {
-      _id: '6',
       name: 'Nike T- Shirt 6',
       category: 'shirt',
       image: '/img/products/p6.jpg',
@@ -90,7 +84,6 @@ const data = {
     },
 
     {
-      _id: '7',
       name: 'Nike T- Shirt 7',
       category: 'shirt',
       image: '/img/products/p7.jpg',
@@ -103,7 +96,6 @@ const data = {
     },
 
     {
-      _id: '8',
       name: 'Nike T- Shirt 8',
       category: 'shirt',
       image: '/img/products/p8.jpg',
--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -8,7 +8,7 @@ const userRouter = experss.Router();
 userRouter.get(
   '/seed',
   expressAsyncHandler(async (req, res) => {
-    await User.remove({}); //--------------------------------- remove all users, this must be remove later --------------------------
+    //await User.remove({}); //--------------------------------- remove all users, this must be remove later --------------------------
     const createdUsers = await User.insertMany(data.users);
     res.send({ createdUsers });
   })
