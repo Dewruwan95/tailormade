caa4bfe1ef1efac38e1899c0d2d76e2baf34c011
Dewruwan95 <dewruwanebay@gmail.com>
2022-05-23 08:35:41 +0530

    fixed some errors

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -23,7 +23,6 @@ const initialState = {
     shippingAddress: localStorage.getItem('shippingAddress')
       ? JSON.parse(localStorage.getItem('shippingAddress'))
       : {},
-
     paymentMethod: 'Paypal',
   },
 };
