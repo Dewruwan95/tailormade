01c1ec854f6aa98c4bfc910164c8ee0fe0cbe475
Dewruwan95 <dewruwanebay@gmail.com>
2022-05-28 08:23:09 +0530

    implement delete order in admin pannel

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -96,7 +96,8 @@ export const deleteProduct = (productId) => async (dispatch, getState) => {
     userSignin: { userInfo },
   } = getState();
   try {
-    const { data } = Axios.delete(`/api/products/${productId}`, {
+    // const { data } = Axios.delete(`/api/products/${productId}`, {  -- if following line dosent work use this line
+    Axios.delete(`/api/products/${productId}`, {
       headers: { Authorization: `Bearer ${userInfo.token}` },
     });
     dispatch({ type: PRODUCT_DELETE_SUCCESS });
